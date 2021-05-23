f50479a1512418b4f4d3f52c00d1d6c20f7b16b4
noikis <ezzil1997@gmail.com>
2021-05-24 02:09:31 +0700

    refactoring bundler

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -1,41 +1,55 @@
-import { FC, useRef, useEffect } from 'react';
+import { useRef, useEffect } from 'react';
 
 interface PreviewProps {
   code: string;
 }
 
 const html = `
-<html>
-  <head></head>
-  <body>
-    <div id="root"></div>
-    <script>
-      window.addEventListener('message', (event) => {
-        try {
-          eval(event.data);
-        } catch (err) {
-          const root = document.querySelector('#root');
-          root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
-          console.error(err);
-        }
-      }, false);
-    </script>
-  </body>
-</html>
-`;
+    <html>
+      <head>
+      <style> 
+      html, body {
+        width: 500px;
+        height: 500px;
+        background: white;
+      }
+      </style
+      </head>
+      <body>    
+        <div id="root"></div>
+        <script>
+ 
+          window.addEventListener('message', (event) => {
+            try {
+        
+               eval(event.data);
+         
+            } catch (err) {
+              const root = document.querySelector('#root');
+              root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+              console.error(err);
+            }
+          }, false);
+        </script>
+      </body>
+    </html>
+  `;
 
-const Preview: FC<PreviewProps> = ({ code }) => {
+const Preview: React.FC<PreviewProps> = ({ code }) => {
   const iframe = useRef<any>();
 
   useEffect(() => {
-    iframe.current.srcdoc = html;
+    //iframe.current.srcdoc = html;
     iframe.current.contentWindow.postMessage(code, '*');
+    console.log('Preview render');
   }, [code]);
 
   return (
     <iframe
-      ref={iframe}
+      width='500'
+      height='500'
       title='preview'
+      ref={iframe}
       sandbox='allow-scripts'
       srcDoc={html}
     />
--- src/index.tsx
+++ src/index.tsx
@@ -1,52 +1,23 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
-
-import * as esbuild from 'esbuild-wasm';
-import { useState, useEffect, useRef } from 'react';
+import { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
-import { fetchPlugin } from './plugins/fetch-plugin';
 import CodeEditor from './components/CodeEditor';
+import bundler from './bundler';
 import Preview from './components/Preview';
 
 const App = () => {
-  const ref = useRef<any>();
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
 
-  const startService = async () => {
-    ref.current = await esbuild.startService({
-      worker: true,
-      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
-    });
-  };
-  useEffect(() => {
-    startService();
-  }, []);
-
   const onClick = async () => {
-    if (!ref.current) {
-      return;
-    }
-
-    const result = await ref.current.build({
-      entryPoints: ['index.js'],
-      bundle: true,
-      write: false,
-      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-      define: {
-        'process.env.NODE_ENV': '"production"',
-        global: 'window',
-      },
-    });
-
-    setCode(result.outputFiles[0]['text']);
+    const output = await bundler(input);
+    setCode(output);
   };
 
   return (
     <div>
       <CodeEditor value={input} onChange={setInput} language='jsx' />
-
       <div>
         <button className='button button-format is-primary' onClick={onClick}>
           Submit
