369f8d671ab806295bacad82c0249db843431bb0
noikis <ezzil1997@gmail.com>
2021-05-24 03:36:05 +0700

    fix resize iframe issue

--- src/components/Resizable.tsx
+++ src/components/Resizable.tsx
@@ -5,8 +5,6 @@ interface ResizableProps {
   direction: 'horizontal' | 'vertical';
 }
 
-// TODO => fix resizing Preview ( watch video 142 )
-
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
   return (
     <ResizableBox height={300} width={Infinity} resizeHandles={['s']}>
