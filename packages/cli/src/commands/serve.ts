41c4e45b85678fe7abe035a5befa129e3ca26301
Ilyas <ezzil1997@gmail.com>
2021-10-06 13:32:42 +0100

    build: Error handling in CLI

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -9,10 +9,27 @@ export const serveCommand = new Command()
   // <number>: required value
   // TODO: display options on command --help
   .option("-p, --port <number>", "port to run server on", "4005")
-  .action((filename = "notebook.js", options: { port: string }) => {
-    const port = parseInt(options.port);
-    // path of the dir where the file is saved
-    const dir = path.join(process.cwd(), path.dirname(filename));
+  .action(async (filename = "notebook.js", options: { port: string }) => {
+    try {
+      const port = parseInt(options.port);
+      // path of the dir where the file is saved
+      const dir = path.join(process.cwd(), path.dirname(filename));
 
-    serve(port, path.basename(filename), dir);
+      await serve(port, path.basename(filename), dir);
+
+      console.log(`Opened ${filename}.`);
+      console.log(
+        `Navigate to https://localhost:${options.port} to edit the file`
+      );
+    } catch (err: any) {
+      // PORT ERROR
+      if (err.code === "EADDRINUSE") {
+        console.error("Port is in use. Try running on a different port.");
+      } else {
+        console.error("Error: ", err.message);
+      }
+
+      // Unsuccessful Run
+      process.exit(1);
+    }
   });
--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -3,7 +3,9 @@ import express from "express";
 export const serve = (port: number, filename: string, dir: string) => {
   const app = express();
 
-  app.listen(port, () => {
-    console.log("Listening on port: ", port);
+  // Wrapping Express listen with a Promise
+  // To handle Errors
+  return new Promise<void>((resolve, reject) => {
+    app.listen(port, resolve).on("error", reject);
   });
 };
